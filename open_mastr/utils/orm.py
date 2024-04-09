7872ae4d95bef2c851f7385ce34e710a4600510d
Florian Kotthoff <74312290+FlorianK13@users.noreply.github.com>
2024-04-09 13:49:32 +0200

    Add new DeclarativeBase sqlalchemy class

--- open_mastr/utils/orm.py
+++ open_mastr/utils/orm.py
@@ -1,5 +1,4 @@
 from sqlalchemy.orm import declarative_base
-from sqlalchemy.schema import MetaData
 from sqlalchemy import (
     Column,
     Integer,
@@ -13,8 +12,8 @@ from sqlalchemy import (
     JSON,
 )
 
-meta = MetaData()
-Base = declarative_base(metadata=meta)
+class Base(DeclarativeBase):
+    pass
 
 
 class ParentAllTables(object):
